4757fc1e9fd3ea57d767e97ccdefe839d0c57ff3
wh1t3d3ath <97808411+wh1t3d3ath@users.noreply.github.com>
2024-03-01 15:24:09 +0200

    Add files via upload

---
+++ Basic CRUD Application/delete-records.php
@@ -0,0 +1,14 @@
+<?php
+include 'db.php';
+
+$id = $_POST['id'];
+
+$sql = "DELETE FROM users WHERE id=$id";
+if ($conn->query($sql) === TRUE) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . $conn->error;
+}
+
+$conn->close();
+?>
